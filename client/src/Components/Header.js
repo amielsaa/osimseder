a4861698440dee2b55b23cb3ac22588fb2594216
Ari <arioshryz@gmail.com>
2024-02-08 15:19:29 +0200

    minor changes

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import './css/Header.css'
+import './css/Header.css';
 import { FaUser } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
 import { Link } from 'react-router-dom';
-const Header = ({userName, role}) => {
+
+const Header = ({ userName, role }) => {
+  const roleBoxStyle = {
+    backgroundColor: role === 'Admin' ? 'red' : '', 
+  };
+
   return (
     <header>
-        
-        <Link to="/home"><div className='logo'>לוגו חברה</div></Link>
-        <h1 className='hello'>שלום {userName}</h1>
-        <div className='icons'>
+      <div className='logo'>לוגו חברה</div>
+      <h1 className='hello'>שלום {userName}</h1>
+      <div className='icons'>
         <IoMdSettings className='settings-icon' />
         <FaUser className='user-icon' />
-        <div className='role-box'>{role}</div>
-        </div>
-      </header>
+        <div className='role-box' style={roleBoxStyle}>{role}</div>
+      </div>
+    </header>
   );
-}
+};
 
 export default Header;
--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -3,31 +3,35 @@ import './css/Nav.css';
 import { Link } from 'react-router-dom';
 
 const Nav = ({ role }) => {
-  const [isSidebarHidden, setIsSidebarHidden] = useState(false);
+  const [isSidebarHidden, setIsSidebarHidden] = useState(true);
 
   const toggleSidebar = () => {
     setIsSidebarHidden(!isSidebarHidden);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarHidden(true);
   };
 
   return (
     <div className="App">
       <nav className={`${isSidebarHidden ? 'hide' : ''}`}>
         <button id="toggle-btn" onClick={toggleSidebar}>
-          <div class="menu-icon"></div>
+          <div className="menu-icon"></div>
           <i className="fa fa-bars"></i>
         </button>
         <span className='menu-title'>תפריט</span>
         <ul className="sidebar-menu">
           {role === 'Admin' && (
             <>
-              <Link to="/Groups"><li><a href="#"><i className="fa fa-home"></i>קבוצות</a></li></Link>
-              <li><a href="#"><i className="fa fa-suitcase"></i>בתים</a></li>
-              <li><a href="#"><i className="fa fa-user"></i>ציוד</a></li>
-              <li><a href="#"><i className="fa fa-gear"></i>מפה</a></li>
+              <li onClick={closeSidebar}><Link to="/Groups"><i className="fa fa-home"></i>קבוצות</Link></li>
+              <li onClick={closeSidebar}><a href="#"><i className="fa fa-suitcase"></i>בתים</a></li>
+              <li onClick={closeSidebar}><a href="#"><i className="fa fa-user"></i>ציוד</a></li>
+              <li onClick={closeSidebar}><a href="#"><i className="fa fa-gear"></i>מפה</a></li>
             </>
           )}
           {role === 'Student' && (
-            <li><a href="#"><i className="fa fa-home"></i>קבוצות</a></li>
+            <li onClick={closeSidebar}><a href="#"><i className="fa fa-home"></i>קבוצות</a></li>
           )}
         </ul>
       </nav>
