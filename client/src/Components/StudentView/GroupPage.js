8a5f61782f9d0c9e65f37280dfdaba7f74bf87f2
Ari <arioshryz@gmail.com>
2024-02-12 14:50:33 +0200

    finished with the group view page

--- client/src/Components/StudentView/GroupPage.js
+++ client/src/Components/StudentView/GroupPage.js
@@ -1,15 +1,41 @@
-// GroupPage.js
+
 import Header from '../Header';
 import Nav from '../Nav';
+import TaskCard from './TaskCard';
 import '../css/GroupPage.css'
 
-import React from 'react';
+import React, { useState } from 'react';
 
-const GroupPage = ({role , userName}) => {
+const GroupPage = ({ role, userName }) => {
+  //Amiel - make sure I get the info like that from Axios request
+  const [studentList, setStudentsList] = useState(['ארי מאיר', 'יואב אביטל', 'פליקס רויזמן', 'עמיאל סעד'])
+  const [tasks, setTasks] = useState([
+    { room: 'סלון', tasks: [
+      { description: 'ניקיון', status: true },
+      { description: 'סידור', status: false },
+    ]},
+    { room: 'חדר ילדים', tasks: [
+      { description: 'תיקון', status: false },
+      { description: 'ניקיון', status: true },
+    ]},
+    { room: 'מטבח', tasks: [
+      { description: 'תיקון', status: false },
+      { description: 'ניקיון', status: true },
+    ]},
+    { room: 'חדר שינה', tasks: [
+      { description: 'תיקון', status: false },
+      { description: 'ניקיון', status: true },
+    ]},
+    { room: 'מחסן', tasks: [
+      { description: 'ניקיון', status: true },
+      { description: 'סידור', status: false },
+    ]},
+  ]);
+
   return (
     <div>
-        <Header userName={userName}  role={role}/>
-        <Nav role={role}/>
+      <Header userName={userName} role={role} />
+      <Nav role={role} />
       <div className='content-Box-Group'>
         <h1>קבוצה: 001</h1>
         <div className='Group-Info'>
@@ -22,20 +48,22 @@ const GroupPage = ({role , userName}) => {
           <h1>חברי הקבוצה</h1>
         </div>
         <div className='Group-Info'>
-          <div className='Info'>ארי מאיר</div>
-          <div className='Info'>פליקס רויזמן</div>
-          <div className='Info'>עמיאל סעד </div>
-          <div className='Info'>יואב אביטל</div>
+          {studentList.map((student, index) => (
+            <div key={index} className='Info'>
+              {student}
+            </div>
+          ))}
         </div>
+
         <div className='group-title'>
-          <h1>מטלות</h1>          
+          <h1>מטלות</h1>
         </div>
-        <div className='Group-Info'>          
+        <div className='Group-Info-Tasks'>
+          {tasks.map((task, index) => (
+            <TaskCard key={index} room={task.room} tasks={task.tasks} />
+          ))}
         </div>
       </div>
-      
-        
-
     </div>
   );
 }
