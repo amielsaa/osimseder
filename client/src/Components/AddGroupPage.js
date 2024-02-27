fccbc387c6d0ccc67da31d3c477aa637401bf646
Ari <arioshryz@gmail.com>
2024-02-27 22:46:14 +0200

    finished all fixed in pages + added  add group buttuon

--- client/src/Components/AddGroupPage.js
+++ client/src/Components/AddGroupPage.js
@@ -7,20 +7,17 @@ import * as Yup from 'yup';
 import DataContext from "../Helpers/DataContext";
 import Footer from "./Footer";
 import { IoChevronForwardCircle } from "react-icons/io5";
-import {fetchAllSchoolsByCity, addGroup} from '../Helpers/StaffFrontLogic'
 
 const AddGroupPage = () => {
   const { user, navigate } = useContext(DataContext);
   const [selectedCity, setSelectedCity] = useState('');
-  const [schoolsList, setSchoolsList] = useState([]);
   const [selectedSchool, setSelectedSchool] = useState('');
   const [selectedCapacity, setSelectedCapacity] = useState('');
 
   const initialValues = {
     city: '',
     school: '',
-    capacity: '',
-    teamOwner: ''
+    capacity: ''
   };
 
   const validationSchema = Yup.object().shape({
@@ -32,26 +29,12 @@ const AddGroupPage = () => {
     capacity: Yup.number().when(['school'], {
       is: (school) => school && school.length > 0,
       then: Yup.number().required('הגבלת כמות נדרשת'),
-    }),
-    teamOwner: Yup.string().when(['capacity'], {
-      is: (capacity) => capacity && capacity.length > 0,
-      then: Yup.string(),
     })
   });
 
   const handleSubmit = () => {
     // Handle form submission
-    const res = addGroup(selectedCity,selectedSchool,selectedCapacity);
-    if(res) {
-      //navigate somewhere
-    } 
   };
-
-  const handleCityChange = async (e) => {
-    setSelectedCity(e.target.value)
-    const schools = await fetchAllSchoolsByCity(e.target.value);
-    setSchoolsList(schools);
-  }
 
   return (
     <>
@@ -72,7 +55,7 @@ const AddGroupPage = () => {
 
               <div>
                 <label htmlFor="city"> עיר: </label>
-                <Field as="select" id="city" name="city" onChange={handleCityChange} value={selectedCity}>
+                <Field as="select" id="city" name="city" onChange={(e) => setSelectedCity(e.target.value)} value={selectedCity}>
                   <option value="">בחר עיר</option>
                   <option value="JRS">ירושלים</option>
                   <option value="BSV">באר שבע</option>
@@ -85,11 +68,8 @@ const AddGroupPage = () => {
                   <label htmlFor="school"> בית ספר: </label>
                   <Field as="select" id="school" name="school" onChange={(e) => setSelectedSchool(e.target.value)} value={selectedSchool}>
                     <option value="">בחר בית ספר</option>
-                    {schoolsList &&
-                      <>{schoolsList.map((school) => (
-                        <option value={school.id}>{school.schoolName}</option>
-                      ))}
-                      </>}
+                    <option value="JRS">נתיבי עם</option>
+                    <option value="BSV">רמבם</option>
                   </Field>
                   <ErrorMessage name="school" component="span" />
                 </div>
@@ -100,26 +80,16 @@ const AddGroupPage = () => {
                   <label htmlFor="capacity"> הגבלה: </label>
                   <Field as="select" id="capacity" name="capacity" onChange={(e) => setSelectedCapacity(e.target.value)} value={selectedCapacity}>
                     <option value="">בחר הגבלה לקבוצה</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
+                    
+                    {Array.from({ length: 9 }, (_, index) => index + 2).map((size) => (
+                      <option key={size} value={size}>{size}</option>
+                    ))}
                   </Field>
                   <ErrorMessage name="capacity" component="span" />
                 </div>
               )}
+                    
 
-              {selectedCapacity && (
-                <div>
-                  <label htmlFor="teamOwner"> חניך גרעין: </label>
-                  <Field as="select" id="teamOwner" name="teamOwner">
-                    <option value="">בחר חניך גרעין (לא חובה)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                  </Field>
-                  <ErrorMessage name="teamOwner" component="span" />
-                </div>
-              )}
 
               {selectedCapacity && (
                 <div className='login_Buttons'>
