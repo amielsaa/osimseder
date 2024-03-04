f4d9ff136c7c74994ff1dd7fd98381a74089d1ef
feliks1 <feliks@post.bgu.ac.il>
2024-03-04 21:16:37 +0200

    added tasks functionality

--- server/routes/staffRoutes/TaskRoute.js
+++ server/routes/staffRoutes/TaskRoute.js
@@ -1,66 +1,73 @@
 const express = require('express');
 const router = express.Router();
-const { TaskModel } = require('../models/TaskModel'); // Import task model
+const {validateToken} = require('../../utils/JsonWebToken')
+const {validateAccess, accessGroup} = require('../../utils/Accesses')
+const StaffTaskLogic = require('../../domain/StaffTaskLogic')
+const {Groups, Staffs, Areas, Schools, Cities, Houses, Tasks} = require('../../models/');
 
 // Create a new task (POST)
-router.post('/', validateToken, async (req, res) => {
+router.post('/', validateToken, validateAccess(accessGroup.C), async (req, res) => {
     try {
-        const task = await TaskModel.create(req.body);
-        res.status(201).json(task);
+        
+        const houseId = req.body.houseId;
+        const type= req.body.type;
+        const room = req.body.room;
+        const freeText = req.body.freeText;
+        const status = req.body.status;
+
+        const newTask = await StaffTaskLogic.createTask(type, room, freeText, status, houseId);
+
+        res.json(newTask);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 });
 
 // Get all tasks (GET)
-router.get('/', validateToken, async (req, res) => {
+router.get('/:houseId', validateToken, validateAccess(accessGroup.C), async (req, res) => {
     try {
-        const tasks = await TaskModel.findAll();
-        res.json(tasks);
+        const houseId = req.params.houseId;
+
+        const newTask = await StaffTaskLogic.getAllTasksByHouse(houseId);
+        res.json(newTask);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
 // Get a single task by ID (GET)
-router.get('/:id', validateToken, async (req, res) => {
+router.post('/:id', validateToken, validateAccess(accessGroup.C), async (req, res) => {
     try {
-        const task = await TaskModel.findByPk(req.params.id);
-        if (task) {
-            res.json(task);
-        } else {
-            res.status(404).json({ message: 'task not found' });
-        }
+        const id = req.params.id;
+
+        const newTask = await StaffTaskLogic.getTaskById(id);
+        res.json(newTask);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
 // Update a task by ID (PUT)
-router.put('/:id', validateToken, async (req, res) => {
+router.put('/:id', validateToken, validateAccess(accessGroup.C), async (req, res) => {
     try {
-        const task = await TaskModel.findByPk(req.params.id);
-        if (task) {
-            await task.update(req.body);
-            res.json(task);
-        } else {
-            res.status(404).json({ message: 'task not found' });
-        }
+        const id = req.params.id;
+        const updatedFields = req.body;
+
+        const newTask = await StaffTaskLogic.updateTask(id, updatedFields);
+        res.json(newTask);
+
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 });
 
 // Delete a task by ID (DELETE)
-router.delete('/:id', validateToken, async (req, res) => {
+router.delete('/:id', validateToken, validateAccess(accessGroup.C), async (req, res) => {
     try {
-        const task = await TaskModel.findByPk(req.params.id);
-        if (task) {
-            await task.destroy();
-            res.json({ message: 'task deleted' });
-        } else {
-            res.status(404).json({ message: 'task not found' });
-        }
+        const id = req.params.id;
+        const newTask = await StaffTaskLogic.deleteTask(id);
+        res.json(newTask);
+
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
