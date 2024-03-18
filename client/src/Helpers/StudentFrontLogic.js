307c4e30f00e73dc0880ed62f46bf25fe13cc0e5
amielsaad <amielsaa@post.bgu.ac.il>
2024-03-18 23:18:22 +0200

    ll

--- client/src/Helpers/StudentFrontLogic.js
+++ client/src/Helpers/StudentFrontLogic.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3001';
+const URL = 'http://localhost:3000/api';
 const headers = {headers: {accessToken: localStorage.getItem('accessToken')}};
 
 
