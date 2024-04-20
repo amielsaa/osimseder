cca50f41079ccd8dba2a768b8db52e02be23b432
Ari <arioshryz@gmail.com>
2024-04-20 22:46:02 +0300

    capacity

--- client/src/Components/AddGroupPage.js
+++ client/src/Components/AddGroupPage.js
@@ -131,7 +131,7 @@ const AddGroupPage = () => {
                   <Field as="select" id="capacity" name="capacity" onChange={(e) => setSelectedCapacity(e.target.value)} value={selectedCapacity}>
                     <option value="">בחר הגבלה לקבוצה</option>
                     
-                    {Array.from({ length: 9 }, (_, index) => index + 2).map((size) => (
+                    {Array.from({ length: 8 }, (_, index) => index + 2).map((size) => (
                       <option key={size} value={size}>{size}</option>
                     ))}
                   </Field>
