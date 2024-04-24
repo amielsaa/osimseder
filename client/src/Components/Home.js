f504c67ead2b1e174973b830655a21576dd700bc
Ari <arioshryz@gmail.com>
2024-04-24 21:40:52 +0300

    Home page done

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,36 +1,67 @@
-import './css/Home.css'
-import Header from './Header'
-import Nav from './Nav'
+import './css/Home.css';
+import Header from './Header';
+import Nav from './Nav';
 import DataContext from '../Helpers/DataContext';
 import { useContext, useEffect, useState } from 'react';
 import Footer from './Footer';
+import useWindowEvents from '../Helpers/useWindowEvents';
+import video_pc_res from '../images/pc_res_video.mp4';
+import video_phone_res from '../images/phone_res_video.mp4';
+
 const Home = () => {
-  
-  const {user,navigate, loginRefresh, setLoginRefresh} = useContext(DataContext)
+  const { user, navigate, loginRefresh, setLoginRefresh, } = useContext(DataContext);
+
+  const windowSize = useWindowEvents()
   useEffect(() => {
-    if(!(localStorage.getItem("accessToken"))){
-      navigate('/404')
+    if (!(localStorage.getItem("accessToken"))) {
+      navigate('/404');
     }
-  })
+  }, []);
+
+  useEffect(() => {}
+,[window.width])
+
   useEffect(() => {
-    if(loginRefresh) {
+    if (loginRefresh) {
       setLoginRefresh(false);
       window.location.reload();
+      
     }
+  }, []);
+  useEffect(() => {
+    
+  }, []);
 
-  },[])
   return (
     <>
-    <Header/>
+      <Header />
+      <Nav />
+      
+      <div className='content-Box-Home'>
+        <div className='overlay'></div>
 
-    <Nav/>
-    
-    <div className='content-Box-Home'>
-          <h1>ברוכים הבאים ותודה על ההשתתפות!</h1>    
-    </div>
-    <Footer/>
-    </>
-  )
-}
+      
+        <video autoPlay loop muted>
+          
+          <source src={windowSize.width >= 768? video_pc_res : video_phone_res} type="video/mp4" />
+          
+          Your browser does not support the video tag.
+        </video>
+        <div className='giant_title'>
+           !ברוכים הבאים
+        </div>
+        <div className='giant_semi_title'>
+           לפרוייקט עושים סדר
+        </div>
+        <div className='giant_semi_semi_title'>
+           תודה על ההשתתפות
+        </div>
+       
+      </div>
+
+      
+    </> 
+  );
+};
 
 export default Home;
