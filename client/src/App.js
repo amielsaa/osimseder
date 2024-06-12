3b153eb57902b35a7e1c5ca895513473cc6e5d20
AriMeir <AriMeir95@gmail.com>
2024-06-12 16:44:04 +0300

    halfway there with locations

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import EditHousePage from './Components/EditHousePage.js'
 import AuthenticateEmailPage from './Components/AuthenticateEmailPage.js'
 import UsersIndex from './Components/UsersIndex.js';
 import StaffIndex from './Components/StaffIndex.js';
+import LocationIndex from './Components/LocationIndex.js';
 
 
 
@@ -47,6 +48,7 @@ function App() {
             <Route path="/addTask/:id" element={<AddTaskPage/>}/>
             <Route path="/students-admin-management" element={<UsersIndex/>}/>
             <Route path="/staff-admin-management" element={<StaffIndex/>}/>
+            <Route path="/admin-location-management" element={<LocationIndex/>}/>
             <Route path="/addGroupToHouse/:id" element={<AddGroupToHousePage/>}/>
             <Route path="/My-Houses" element={<Houses />} />
             <Route path="/HousePage/:id" element={<HousePage />} />
--- client/src/Components/StaffIndex.js
+++ client/src/Components/StaffIndex.js
@@ -3,196 +3,117 @@ import './css/UsersIndex.css';
 import Header from './Header';
 import Footer from './Footer';
 import Nav from './Nav';
-import UsersTable from './UsersTable';
-import JoinGroupWindow from './JoinGroupWindow';
 import DataContext from '../Helpers/DataContext';
 import ConfirmationMessage from './ConfirmationMessage';
+import StaffTable from './StaffTable';
 
 const exampleUsers = [
-    {
-      name: 'יוחנן',
-      lastName: 'כהן',
-      phoneNumber: '123-456-7890',
-      parentName: 'שרה כהן',
-      parentPhoneNumber: '098-765-4321',
-      city: 'תל אביב',
-      school: 'תיכון מרכזי',
-      gender: 'זכר',
-      language: 'עברית',
-      groupId: 1
-    },
-    {
-      name: 'אמה',
-      lastName: 'לוי',
-      phoneNumber: '234-567-8901',
-      parentName: 'משה לוי',
-      parentPhoneNumber: '198-765-4321',
-      city: 'ירושלים',
-      school: 'תיכון מערבי',
-      gender: 'נקבה',
-      language: 'אנגלית',
-      groupId: 2
-    },
-    {
-      name: 'נועם',
-      lastName: 'ישראלי',
-      phoneNumber: '345-678-9012',
-      parentName: 'הילה ישראלי',
-      parentPhoneNumber: '297-654-3210',
-      city: 'חיפה',
-      school: 'תיכון צפון',
-      gender: 'זכר',
-      language: 'צרפתית',
-      groupId: undefined
-    },
-    {
-      name: 'יעל',
-      lastName: 'בראון',
-      phoneNumber: '456-789-0123',
-      parentName: 'יהודה בראון',
-      parentPhoneNumber: '396-543-2109',
-      city: 'באר שבע',
-      school: 'תיכון דרום',
-      gender: 'נקבה',
-      language: 'גרמנית',
-      groupId: 3
-    },
-    {
-      name: 'איתי',
-      lastName: 'שמעוני',
-      phoneNumber: '567-890-1234',
-      parentName: 'מיכל שמעוני',
-      parentPhoneNumber: '495-432-1098',
-      city: 'אשדוד',
-      school: 'תיכון מזרח',
-      gender: 'זכר',
-      language: 'איטלקית',
-      groupId: undefined
-    },
-    {
-      name: 'ליה',
-      lastName: 'גרסיה',
-      phoneNumber: '678-901-2345',
-      parentName: 'דוד גרסיה',
-      parentPhoneNumber: '594-321-0987',
-      city: 'פתח תקווה',
-      school: 'תיכון מערב',
-      gender: 'נקבה',
-      language: 'פורטוגזית',
-      groupId: 1
-    },
-    {
-      name: 'אילן',
-      lastName: 'מרטינז',
-      phoneNumber: '789-012-3456',
-      parentName: 'לינדה מרטינז',
-      parentPhoneNumber: '693-210-9876',
-      city: 'ראשון לציון',
-      school: 'תיכון מרכזי',
-      gender: 'זכר',
-      language: 'סינית',
-      groupId: undefined
-    },
-    {
-      name: 'אביגיל',
-      lastName: 'הרננדז',
-      phoneNumber: '890-123-4567',
-      parentName: 'כריסטופר הרננדז',
-      parentPhoneNumber: '792-109-8765',
-      city: 'רמת גן',
-      school: 'תיכון צפון',
-      gender: 'נקבה',
-      language: 'יפנית',
-      groupId: 2
-    },
-    {
-      name: 'ויליאם',
-      lastName: 'לופז',
-      phoneNumber: '901-234-5678',
-      parentName: 'ברברה לופז',
-      parentPhoneNumber: '891-098-7654',
-      city: 'נתניה',
-      school: 'תיכון דרום',
-      gender: 'זכר',
-      language: 'קוריאנית',
-      groupId: 3
-    },
-    {
-      name: 'סופיה',
-      lastName: 'גונזלס',
-      phoneNumber: '012-345-6789',
-      parentName: 'ריצ\'רד גונזלס',
-      parentPhoneNumber: '990-987-6543',
-      city: 'הרצליה',
-      school: 'תיכון מזרח',
-      gender: 'נקבה',
-      language: 'רוסית',
-      groupId: undefined
-    }
-  ];
-  const exampleGroups = [
-    {
-        groupId: 1,
-        capacity: 10,
-        membersCount: 7
-    },
-    {
-        groupId: 2,
-        capacity: 15,
-        membersCount: 12
-    },
-    {
-        groupId: 3,
-        capacity: 20,
-        membersCount: 20
-    },
-    {
-        groupId: 4,
-        capacity: 25,
-        membersCount: 18
-    },
-    {
-        groupId: 5,
-        capacity: 30,
-        membersCount: 25
-    },
-    {
-        groupId: 6,
-        capacity: 12,
-        membersCount: 10
-    },
-    {
-        groupId: 7,
-        capacity: 18,
-        membersCount: 15
-    },
-    {
-        groupId: 8,
-        capacity: 22,
-        membersCount: 20
-    },
-    {
-        groupId: 9,
-        capacity: 14,
-        membersCount: 8
-    },
-    {
-        groupId: 10,
-        capacity: 50,
-        membersCount: 45
-    }
+  {
+    name: 'יוחנן',
+    lastName: 'כהן',
+    role: 'TeamOwner',
+    phoneNumber: '123-456-7890',
+    city: 'תל אביב',
+    gender: 'זכר',
+    confirmationStatus: true
+  },
+  {
+    name: 'אמה',
+    lastName: 'לוי',
+    role: 'AreaManager',
+    phoneNumber: '234-567-8901',
+    city: 'ירושלים',
+    gender: 'נקבה',
+    confirmationStatus: false,
+    area: 'Jerusalem'
+  },
+  {
+    name: 'נועם',
+    lastName: 'ישראלי',
+    role: 'AreaManager',
+    phoneNumber: '345-678-9012',
+    city: 'חיפה',
+    gender: 'זכר',
+    confirmationStatus: true,
+    area: 'Haifa'
+  },
+  {
+    name: 'יעל',
+    lastName: 'בראון',
+    role: 'CityManager',
+    phoneNumber: '456-789-0123',
+    city: 'באר שבע',
+    gender: 'נקבה',
+    confirmationStatus: false
+  },
+  {
+    name: 'איתי',
+    lastName: 'שמעוני',
+    role: 'TeamOwner',
+    phoneNumber: '567-890-1234',
+    city: 'אשדוד',
+    gender: 'זכר',
+    confirmationStatus: true
+  },
+  {
+    name: 'דניאל',
+    lastName: 'גולן',
+    role: 'AreaManager',
+    phoneNumber: '678-901-2345',
+    city: 'תל אביב',
+    gender: 'זכר',
+    confirmationStatus: true,
+    area: 'Tel Aviv'
+  },
+  {
+    name: 'שירה',
+    lastName: 'לוין',
+    role: 'CityManager',
+    phoneNumber: '789-012-3456',
+    city: 'חיפה',
+    gender: 'נקבה',
+    confirmationStatus: true
+  },
+  {
+    name: 'אלעד',
+    lastName: 'גרין',
+    role: 'CityManager',
+    phoneNumber: '890-123-4567',
+    city: 'ירושלים',
+    gender: 'זכר',
+    confirmationStatus: false
+  },
+  {
+    name: 'איתן',
+    lastName: 'רז',
+    role: 'AreaManager',
+    phoneNumber: '901-234-5678',
+    city: 'באר שבע',
+    gender: 'זכר',
+    confirmationStatus: true,
+    area: 'Beersheba'
+  },
+  {
+    name: 'מאיה',
+    lastName: 'כהן',
+    role: 'TeamOwner',
+    phoneNumber: '012-345-6789',
+    city: 'תל אביב',
+    gender: 'נקבה',
+    confirmationStatus: false
+  }
 ];
+
+  
 
 const StaffIndex = () => {
   const [users, setusers] = useState(exampleUsers)
-  const [groups, setGroups] = useState(exampleGroups)
   const [showConfirmation, setShowConfirmation] = useState(false);
-  const [selectedUserToGroup, setSelectedUserToGroup] = useState(null)
-  const [selectedGroupId, setSelectedGroupId] = useState(null);
+  const [showConfirmationUserDeletion, setShowConfirmationUserDeletion] = useState(false);
+  const [showConfirmationUserAcceptance, setShowConfirmationUserAcceptance] = useState(false);
   const [filter, setFilter] = useState('');
-  const [showGroupsWindow, setShowGroupsWindow] = useState(false);
   const [filterType, setFilterType] = useState('name');
-  const [chosenStudent, setChosenStudent] = useState('');
+  const [chosenStaff, setChosenStaff] = useState('');
   const { navigate, user } = useContext(DataContext);
 
   
@@ -205,42 +126,54 @@ const StaffIndex = () => {
     setFilterType(e.target.value);
   };
 
-  function handleConfirmation() {
-    //Yoav - this is the logic where you put a student into group
-    setShowConfirmation(false)
-    setShowGroupsWindow(false)
+  
+  
+  function onApproveStaffMember(userId, userName, userLastName, role) {
+    const staff = {
+      userId: userId,
+      userName: userName,
+      userLastName: userLastName,
+      role: role
+  }
+    setChosenStaff(staff)
+    setShowConfirmationUserAcceptance(true)
+
+  }
+  
+  
+  function prepareDeleteStaffAction(userId, userName, userLastName) {
+    
+     const staff = {
+       userId: userId,
+       userName: userName,
+       userLastName: userLastName,
+   }
+     
+     setChosenStaff(staff)
+     setShowConfirmationUserDeletion(true)
+   }
+ 
+
+
+  function handleDeleteUserConfirmation () {
+    // deletes chosenUser from the system
     console.log("Im here!")
+    setShowConfirmationUserDeletion(false)
+  }
+  function handleAcceptUserConfirmation () {
+    // Accepts chosenUser from the system
+    console.log("Im here!")
+    setShowConfirmationUserAcceptance(false)
   }
 
-  const prepareJoinGroupAction = (userId, userName, userLastName, schoolName) => {
-    const student = {
-        userId: userId,
-        userName: userName,
-        userLastName: userLastName,
-        schoolName: schoolName
-    }
-    setChosenStudent(student)
-    console.log("Im here!")
-    setShowGroupsWindow(true);
-    
-  };
-  function onClose () {
-    setShowGroupsWindow(false)
-  }
-  function onDeleteStudent(userId) {
-    // yoav - deletes user from the system
-  }
-  function onAddToGroup(groupId, userId, userName, userLastName) {
-    setSelectedGroupId(groupId);
-    setSelectedUserToGroup(userName + " " + userLastName)
-    setShowConfirmation(true);
-  }
+
+  
 
   const filteredUsers = users.filter((user) => {
     if (filterType === 'name') {
       return user.name.includes(filter);
-    } else if (filterType === 'school') {
-      return user.school.includes(filter);
+    } else if (filterType === 'role') {
+      return user.role.toLowerCase().includes(filter.toLowerCase());
     } else if (filterType === 'city') {
       return user.city.includes(filter);
     }
@@ -253,7 +186,7 @@ const StaffIndex = () => {
       <Nav />
       <div className='content-box-users-index'>
         <div className='users-title'>
-          <h1>טבלת חניכים</h1>
+          <h1>טבלת סגל</h1>
         </div>
         <div className='filter-container'>
           <div className='filter-box'>
@@ -265,19 +198,32 @@ const StaffIndex = () => {
             />
             <select value={filterType} onChange={handleFilterTypeChange}>
                 <option value='name'>שם</option>
-                <option value='school'>בית ספר</option>
+                <option value='role'>תפקיד</option>
                 <option value='city'>עיר</option>
             </select>
           </div>
         </div>
-        <UsersTable users={filteredUsers}  prepareJoinGroupAction={prepareJoinGroupAction} onDeleteStudent={onDeleteStudent} />
+
+
+
+
+        <StaffTable users={filteredUsers} prepareDeleteStaffAction={prepareDeleteStaffAction} onApproveStaffMember={onApproveStaffMember} />
       </div>
-      {showGroupsWindow &&  <JoinGroupWindow student={chosenStudent} groups={groups} onClose={onClose} onAddToGroup={onAddToGroup}  />}
-      {showConfirmation && (
+
+
+
+      {showConfirmationUserDeletion && (
         <ConfirmationMessage
-          confirmationMessage={`לשייך את ${selectedUserToGroup} לקבוצה מספר ${selectedGroupId}`}
-          handleConfirmation={handleConfirmation}
-          setShowConfirmation={setShowConfirmation}
+          confirmationMessage={`האם למחוק את ${chosenStaff.userName + " " + chosenStaff.userLastName } מהמערכת?`}
+          handleConfirmation={handleDeleteUserConfirmation}
+          setShowConfirmation={setShowConfirmationUserDeletion}
+        />
+      )}
+      {showConfirmationUserAcceptance && (
+        <ConfirmationMessage
+          confirmationMessage={`האם לאשר את ${chosenStaff.userName + " " + chosenStaff.userLastName } לתפקיד ${chosenStaff.role} במערכת?`}
+          handleConfirmation={handleAcceptUserConfirmation}
+          setShowConfirmation={setShowConfirmationUserDeletion}
         />
       )}
       <Footer />
