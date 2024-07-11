941eaf6992ef67fd2097a8ef6e2f102b5114cdbf
AriMeir <AriMeir95@gmail.com>
2024-07-11 18:05:31 +0300

    finished

--- client/src/Components/ScoreBar.js
+++ client/src/Components/ScoreBar.js
@@ -4,13 +4,23 @@ function ScoreBar({ numberOfCompletedTasks, numberOfTasks }) {
 
     const calculateWidth = () => {
         if (numberOfTasks === 0) {
+            return '0%';
+        } else {
+            const width = ((numberOfCompletedTasks / numberOfTasks) * 100).toFixed(2);
+            return `${width}%`;
+        }
+    };
+
+    const getScore = () => {
+        if (numberOfTasks === 0) {
             return 'טרם נוצרו מטלות';
         } else {
-            return `${(numberOfCompletedTasks / numberOfTasks) * 100}%`;
+            return `${numberOfCompletedTasks}/${numberOfTasks}`;
         }
     };
 
     const width = calculateWidth();
+    const score = getScore();
 
     return (
         <div className='review-bar-and-score flex align-center row'>
@@ -19,10 +29,10 @@ function ScoreBar({ numberOfCompletedTasks, numberOfTasks }) {
                     <div className='review-bar-out-of'>
                         <div className='review-bar-score' style={{ width }}></div>
                     </div>
-                    <span className='score'>{width}</span>
+                    <span className='score'>{score}</span>
                 </>
             ) : (
-                <span className='no-tasks-indicator'>טרם נוצרו מטלות</span>
+                <span className='no-tasks-indicator'>{score}</span>
             )}
         </div>
     );
--- client/src/Components/TeamOwnerView/GroupListTO.js
+++ client/src/Components/TeamOwnerView/GroupListTO.js
@@ -1,69 +1,65 @@
 import React, { useContext, useEffect, useState } from 'react';
 import GroupTO from './GroupTO';
 import DataContext from '../../Helpers/DataContext';
-import {fetchAllGroupsStaff} from '../../Helpers/StaffFrontLogic'
-const GroupListTO = ({selectedCity, selectedSchool}) => {
+import { fetchAllGroupsStaff } from '../../Helpers/StaffFrontLogic';
+
+const GroupListTO = ({ selectedCity, selectedSchool }) => {
   const { user } = useContext(DataContext);
+  const [groups, setGroups] = useState([]);
+  const [filteredGroups, setFilteredGroups] = useState([]);
 
-  
-  const [groups, setGroupIds] = useState([]);
-  const [filteredGroups, setFilteredGroups] = useState([])
-  
-  
-
-
-
-
-    
-
-    useEffect(() => {
-    const setGroups = async () => {
+  useEffect(() => {
+    const fetchGroups = async () => {
       const groups = await fetchAllGroupsStaff();
-      setGroupIds(groups);
-      FilterGroups()
+      setGroups(groups);
     };
-    setGroups();
+    fetchGroups();
+  }, [user]);
 
-  }, [user,selectedCity, selectedSchool]);  
+  useEffect(() => {
+    const filterGroups = () => {
+      let filteredGroupsList = groups;
 
-  
+      if (selectedCity) {
+        console.log('city');
+        filteredGroupsList = filteredGroupsList.filter(
+          group => parseInt(group.cityId) === parseInt(selectedCity)
+        );
+      }
 
-  const deleteGroupFromList = (id) => {
-    setGroupIds(groupIds => groupIds.filter(group => group.id !== id));
-  }
-  // Amiel - this is the new function that filters groups
-  function FilterGroups() {
-    let filteredGroupsList = groups;
+      if (selectedSchool) {
+        console.log('school');
+        filteredGroupsList = filteredGroupsList.filter(
+          group => parseInt(group.schoolId) === parseInt(selectedSchool)
+        );
+      }
 
-    if (selectedCity) {
-      console.log("city")
-      filteredGroupsList = filteredGroupsList.filter(group => parseInt(group.cityId) == parseInt(selectedCity));
-    }
+      setFilteredGroups(filteredGroupsList);
+    };
 
-    if (selectedSchool) {
-      console.log("neighborhood")
-      filteredGroupsList = filteredGroupsList.filter(group => parseInt(group.schoolId) == parseInt(selectedSchool));
-    }
+    filterGroups();
+  }, [groups, selectedCity, selectedSchool]);
 
-    setFilteredGroups(filteredGroupsList);
-
-  }
-
-
+  const deleteGroupFromList = id => {
+    setGroups(groups => groups.filter(group => group.id !== id));
+  };
 
   return (
-    <> 
-      {/*Amiel - should be filteredGroups here instead of groups. test it out when you finish with all the back stuff. */}
-      { (filteredGroups.length > 0) && <>
-      {filteredGroups.map((groupJson) => (
-        <GroupTO key={groupJson.id}
-                groupId={groupJson.id}
-                groupJson={groupJson}
-                deleteGroupFromList={deleteGroupFromList} />
-      ))} </>
-      }
+    <>
+      {filteredGroups.length > 0 && (
+        <>
+          {filteredGroups.map(groupJson => (
+            <GroupTO
+              key={groupJson.id}
+              groupId={groupJson.id}
+              groupJson={groupJson}
+              deleteGroupFromList={deleteGroupFromList}
+            />
+          ))}
+        </>
+      )}
     </>
   );
-}
+};
 
 export default GroupListTO;
